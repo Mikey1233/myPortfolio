f106472babca5d185e54a96cfbfa3269cc60e598
michael1233 <emmamichael201@gmail.com>
2023-12-12 13:59:12 +0100

    configured my pwa files and fix some bugs

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,21 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react(), VitePWA({})],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // Automatically register the PWA for updates
+      manifest: {
+        name: 'Michael Emmanuel',
+        short_name: 'Mikey',
+        icons: [
+          {
+            src: '/Assets/star-2.svg',
+            sizes: '192x192',
+            type: 'image/svg',
+          },
+        ],
+      },
+    }),
+  ],
 });
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
